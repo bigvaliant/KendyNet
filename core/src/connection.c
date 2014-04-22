846851247bd71cd217db62525ad1be6c0881751a
HuangWei <sniper@ubuntu.ubuntu-domain>
2014-04-22 22:15:56 +0800

    none

--- core/src/connection.c
+++ core/src/connection.c
@@ -177,6 +177,11 @@ int32_t send_packet(struct connection *c,wpacket_t w)
 		wpk_destroy(&w);
 		return -1;
 	}
+	if(*w->len > c->recv_bufsize){
+		//包长度超大，记录日志
+		wpk_destroy(&w);
+		return 0;
+	}
 	st_io *O;
 	if(w){
 		w->base.tstamp = GetSystemMs64();
